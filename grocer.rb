86898140e4e7bf104552ad8a521b1519da97867a
Saige Leslie <saigeleslie@gmail.com>
2018-04-18 06:55:14 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ end
 
 def checkout(cart, coupons)
   consolidated = consolidate_cart(cart)
-  binding.pry
+  
   with_coupons = apply_coupons(consolidated, coupons)
  
   clearance = apply_clearance(with_coupons)
